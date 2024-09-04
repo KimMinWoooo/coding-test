441ff35c0177816167c0ec93054be4c7cb290c34
김민우 <122437698+KimMinWoooo@users.noreply.github.com>
2024-09-04 19:13:25 +0900

    [Bronze III] Title: 직각삼각형, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,15 @@
+n = []
+
+while True:
+    a, b, c = map(int, input().split())
+    
+    if a == 0 and b == 0 and c == 0:
+        break
+
+    if a*a + b*b == c*c or a*a + c*c == b*b or b*b + c*c == a*a:
+        n.append('right\n')
+    else:
+        n.append('wrong\n')
+
+
+print(''.join(n))
