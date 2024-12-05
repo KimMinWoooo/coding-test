35a517d54c9ee50dabf0a176b1aa06d11b99df78
김민우 <122437698+KimMinWoooo@users.noreply.github.com>
2024-12-05 17:49:48 +0900

    [Gold IV] Title: 다이어트, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/19942. 다이어트/다이어트.py
+++ 백준/Gold/19942. 다이어트/다이어트.py
@@ -1,35 +1,35 @@
-def recur(idx, p,f,s,v,c):
+def recur(idx,p,f,s,v,c):
     global answer
-    global used
-    global answer_used
+    global use
+    global ans_use
 
-    
-    if mp <= p and mf <= f and ms <= s and mv <= v:
-            if answer > c:
-                answer = min(answer, c)
-                answer_used = []
-                for i in used:
-                    answer_used.append(i)
-    if idx == n:
+    if p >= mp and f >=mf and s >= ms and v >= mv:
+        if answer > c:
+            answer = min(answer, c)
+            ans_use = []
+            for i in use:
+                ans_use.append(i)
+    if idx == N:
         return
-
-    used.append(idx+1)
-    recur(idx + 1, p+foods[idx][0], f+foods[idx][1], s+foods[idx][2], v+foods[idx][3], c+foods[idx][4])
-    used.pop()
-    recur(idx + 1,p,f,s,v,c)
     
+    # 식재료 합을 한 경우
+    use.append(idx+1)
+    recur(idx+1, p+foods[idx][0], f+foods[idx][1], s+foods[idx][2], v+foods[idx][3], c+foods[idx][4])
+    use.pop()
+    recur(idx+1,p,f,s,v,c)
 
-n = int(input())
-mp,mf,ms,mv = map(int, input().split())
-foods = [list(map(int, input().split())) for _ in range(n)]
+N = int(input())
+
+mp, mf, ms, mv = map(int, input().split())
+
+foods = [list(map(int, input().split())) for _ in range(N)]
 
 answer = 1e9
-used = []
-answer_used = []
+use = []
+ans_use = []
 recur(0,0,0,0,0,0)
-
-if answer_used:
+if ans_use:
     print(answer)
-    print(*answer_used)
+    print(*ans_use)
 else:
     print(-1)
