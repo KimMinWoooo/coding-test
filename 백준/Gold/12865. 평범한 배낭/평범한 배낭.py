cbdbee12cfad077f0b79c000c36dc4db8b2cafae
김민우 <122437698+KimMinWoooo@users.noreply.github.com>
2024-12-06 18:31:13 +0900

    [Gold V] Title: 평범한 배낭, Time: 1744 ms, Memory: 158936 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,20 @@
+# 탑 바텀 dp로 푼 넵섹 문제
+
+def recur(idx, weight):
+    if weight > K:
+        return -99999999
+    if idx == N:
+        return 0
+    if dp[idx][weight] != -1:
+        return dp[idx][weight]
+
+    dp[idx][weight] = max(recur(idx+1, weight + arr[idx][0])+ arr[idx][1], recur(idx+1, weight))
+
+    return dp[idx][weight]
+
+N, K = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+dp = [[-1 for _ in range(100_001)] for _ in range(N)]
+
+ans = recur(0, 0)
+print(ans)
