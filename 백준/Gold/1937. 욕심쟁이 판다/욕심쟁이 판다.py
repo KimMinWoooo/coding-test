1b45393df9af34f9b4fced5b3114c8464ed52aa8
김민우 <122437698+KimMinWoooo@users.noreply.github.com>
2024-12-08 17:53:23 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 376 ms, Memory: 142236 KB -BaekjoonHub

---
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
@@ -0,0 +1,25 @@
+def recur(y, x):
+    if dp[y][x] != 0:
+        return dp[y][x]
+    
+    for dy, dx in [[1, 0],[-1, 0],[0, 1],[0, -1]]:
+        ey = y+dy
+        ex = x+dx
+
+        if 0 <= ex < n and 0 <= ey < n:
+            if arr[y][x] < arr[ey][ex]:
+                dp[y][x] = max(dp[y][x], recur(ey,ex) + 1)
+    return dp[y][x]
+
+
+
+import sys
+input = sys.stdin.readline
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+dp = [[0 for _ in range(n)] for _ in range(n)]
+for y in range(n):
+    for x in range(n):
+        recur(y, x)
+
+print(max(map(max, dp))+1)
