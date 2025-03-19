76a79cc5553a0c40e0783be5f6b9593df26869d5
김민우 <122437698+KimMinWoooo@users.noreply.github.com>
2025-03-19 18:32:03 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,36 +1,36 @@
+import sys
 from collections import deque
+
+input = sys.stdin.readline
 
 N, M = map(int, input().split())
 
-maze = []
+arr = []
 for _ in range(N):
-    row = list(map(int, input().strip()))
-    maze.append(row)
+    row = list(map(int,input().strip()))
+    arr.append(row)
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
 
-def bfs():
-    queue = deque()
-    queue.append((0, 0))
-    
-    while queue:
-        x, y = queue.popleft()
-        
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            
-            if nx < 0 or nx >= N or ny < 0 or ny >= M:
-                continue
-            
-            if maze[nx][ny] == 0:
-                continue
-            
-            if maze[nx][ny] == 1:
-                maze[nx][ny] = maze[x][y] + 1
-                queue.append((nx, ny))
-    
-    return maze[N-1][M-1]
+q = deque()
+q.append((0,0))
 
-print(bfs())
+while len(q) > 0:
+    x, y = q.popleft()
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if nx < 0 or nx >= N or ny < 0 or ny >= M:
+            continue
+
+        if arr[nx][ny] == 0:
+            continue
+
+        if arr[nx][ny] == 1:
+            arr[nx][ny] = arr[x][y] + 1
+            q.append((nx,ny))
+
+print(arr[N-1][M-1])
