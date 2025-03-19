ecaa1e7f04d5e6ccfd6e1ebe1121ee100ce15c1a
김민우 <122437698+KimMinWoooo@users.noreply.github.com>
2025-03-19 16:15:07 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 596 ms, Memory: 71096 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,32 @@
+import sys
+sys.setrecursionlimit(10**6) 
+input = sys.stdin.readline
+
+N, M, R = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+visited = [False] * (N+1)
+result = [0] * (N+1)
+for _ in range(M):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+for i in range(1, N+1):
+    graph[i].sort()
+
+order = 1
+
+def recur(node):
+    global order
+    visited[node] = True
+    result[node] = order
+    order += 1
+    for next in graph[node]:
+        if visited[next] == False:
+            recur(next)
+
+
+recur(R)
+
+for i in range(1, N+1):
+    print(result[i])
