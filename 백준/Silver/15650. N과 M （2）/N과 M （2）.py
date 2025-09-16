ea9d774f7371233956a0ec6d255f112c29b4809e
김민우 <122437698+KimMinWoooo@users.noreply.github.com>
2025-09-16 22:43:19 +0900

    [Silver III] Title: N과 M (2), Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,17 +1,16 @@
-N, M = list(map(int,input().split()))
+def solution(S, K):
+    if K == M:
+        print(*result)
+        return
+    for i in range(S, N+1):
+        if i in result:
+            continue
+        result.append(i)
+        solution(i + 1, K + 1)
+        result.pop()
+
+N, M = map(int, input().split())
+
 result = []
 
-def dfs(start):
-    # 조건 : 수열의 길이가 M개까지 골라졌다면
-    if (len(result) == M):
-        print(' '.join(map(str, result)))
-        return
-    # 반복 : start 부터 N + 1 까지
-    for i in range(start, N + 1):
-        # 조건 : result 수열안에 i가 없으면
-        # result에 i를 추가하고 dfs를 추가 재귀 호출 -> 끝나면 마지막 숫자를 pop해서 제거함
-        if i not in result:
-            result.append(i)
-            dfs(i + 1)
-            result.pop()
-dfs(1)
+solution(1, 0)
